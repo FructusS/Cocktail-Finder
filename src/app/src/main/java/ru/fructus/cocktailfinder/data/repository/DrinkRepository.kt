7a60f76bf972ccc99a4be391f1569c24e757bfa2
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:33:57 +0300

    inject constructor

--- src/app/src/main/java/ru/fructus/cocktailfinder/data/repository/DrinkRepository.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/data/repository/DrinkRepository.kt
@@ -2,10 +2,11 @@ package ru.fructus.cocktailfinder.data.repository
 
 import ru.fructus.cocktailfinder.data.remote.DrinkApi
 import ru.fructus.cocktailfinder.domain.DrinkDto
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DrinkRepository(private val drinkApi: DrinkApi) {
+class DrinkRepository @Inject constructor(private val drinkApi: DrinkApi) {
 
     suspend fun getRandomDrinkList() : List<DrinkDto>{
         return drinkApi.getRandomDrinkList().map { it.toDrinkDto() }
