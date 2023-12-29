715a06e4a0182a4ad89e8b5a9fc277bbe2c37cf4
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:28:34 +0300

    change response model on DrinkListEntity and add route

--- src/app/src/main/java/ru/fructus/cocktailfinder/data/remote/DrinkApi.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/data/remote/DrinkApi.kt
@@ -2,10 +2,11 @@ package ru.fructus.cocktailfinder.data.remote
 
 import retrofit2.http.GET
 import ru.fructus.cocktailfinder.domain.DrinkEntity
+import ru.fructus.cocktailfinder.domain.DrinkListEntity
 
 interface DrinkApi {
 
-    @GET
-    suspend fun getRandomDrinkList() : List<DrinkEntity>
+    @GET("random.php")
+    suspend fun getRandomDrinkList() : DrinkListEntity?
 
 }
