2dc8a41157ac0aeb402cd5121f62b86c13c87415
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:39:28 +0300

    show progress indicator when getRandomDrinkList loading

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenLoading.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenLoading.kt
@@ -1,8 +1,10 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
-fun DrinkListScreenLoading(){
-
+fun DrinkListScreenLoading(state: DrinkListContract.State) {
+    CircularProgressIndicator()
 }
