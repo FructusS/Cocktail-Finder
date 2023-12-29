a98c6c9cae4d1fb75224c77d4be05c8dd55ce4e5
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:37:38 +0300

    implement BaseContract.kt and add fun getRandomDrink

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListViewModel.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListViewModel.kt
@@ -1,8 +1,66 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import ru.fructus.cocktailfinder.data.repository.DrinkRepository
+import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
+import javax.inject.Inject
+
 @HiltViewModel
-class DrinkListViewModel(private val drinkRepository: DrinkRepository) {
+class DrinkListViewModel @Inject constructor(private val drinkRepository: DrinkRepository) : ViewModel(),
+    DrinkListContract {
+
+    private val _state = MutableStateFlow<DrinkListContract.State>(DrinkListContract.State.Loading)
+    override val state: StateFlow<DrinkListContract.State> = _state.asStateFlow()
+    override val effect: SharedFlow<DrinkListContract.Effect>
+        get() = TODO("Not yet implemented")
+
+    override fun event(event: DrinkListContract.Event) = when (event) {
+        DrinkListContract.Event.OnBackPressed -> TODO()
+        DrinkListContract.Event.OnEnterScreen -> {
+            getRandomDrink()
+        }
+
+        is DrinkListContract.Event.OnFavoriteClick -> TODO()
+        DrinkListContract.Event.OnRefresh -> TODO()
+        is DrinkListContract.Event.ShowToast -> TODO()
+    }
+
+    private fun getRandomDrink() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _state.update {
+                DrinkListContract.State.Loading
+            }
+            try {
+                val response = drinkRepository.getRandomDrinkList()
+                if (response.isNullOrEmpty()) {
+                    _state.update {
+                        DrinkListContract.State.NoItems
+
+                    }
+                } else {
+                    _state.update {
+                        DrinkListContract.State.Success(response)
+                    }
+                }
+
+            } catch (e: Exception) {
+                _state.update {
+                    DrinkListContract.State.Error(e.localizedMessage)
+                }
+            }
+        }
+    }
 
 }
+
+
+
