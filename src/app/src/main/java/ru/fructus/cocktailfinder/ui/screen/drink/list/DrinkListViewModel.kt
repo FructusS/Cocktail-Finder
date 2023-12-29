f66bd43d0767882d9f6ed3b1ef2ddaba54720833
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-29 15:46:35 +0300

    create view model for drink list screen

---
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListViewModel.kt
@@ -0,0 +1,8 @@
+package ru.fructus.cocktailfinder.ui.screen.drink.list
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.fructus.cocktailfinder.data.repository.DrinkRepository
+@HiltViewModel
+class DrinkListViewModel(private val drinkRepository: DrinkRepository) {
+
+}
