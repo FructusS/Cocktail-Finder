1c16d1b0a92fce89cc78cfd55254ef8dd71ba7a1
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-29 14:07:29 +0300

    module for retrofit

---
+++ src/app/src/main/java/ru/fructus/cocktailfinder/di/RetrofitModule.kt
@@ -0,0 +1,41 @@
+package ru.fructus.cocktailfinder.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import ru.fructus.cocktailfinder.data.remote.DrinkApi
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS).retryOnConnectionFailure(true)
+            .callTimeout(30, TimeUnit.SECONDS).addNetworkInterceptor(
+                HttpLoggingInterceptor { message ->
+                    println("LOG-NET: $message")
+                }.apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                },
+            ).build()
+    }
+
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl("www.thecocktaildb.com/api/json/v1/1/random.php").build()
+    }
+
+
+    @Provides
+    fun provideDrinkApi(retrofit: Retrofit) {
+        retrofit.create(DrinkApi::class.java)
+    }
+}
