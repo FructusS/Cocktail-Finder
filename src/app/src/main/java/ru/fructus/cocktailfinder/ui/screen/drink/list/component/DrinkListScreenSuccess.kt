ff1b1ebedd60b902c676ab86cba6cd635e0a4395
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:38:22 +0300

    add text for showing drink list

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenSuccess.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenSuccess.kt
@@ -1,9 +1,11 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
 
-fun DrinkListScreenSuccess(){
-
+fun DrinkListScreenSuccess(state: DrinkListContract.State.Success) {
+    Text(text = state.list.toString())
 }
