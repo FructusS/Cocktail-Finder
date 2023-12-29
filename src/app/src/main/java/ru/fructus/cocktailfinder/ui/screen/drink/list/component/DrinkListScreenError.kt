c3d730406487d101979374965dce6e5063592a8b
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:40:18 +0300

    show error when getRandomDrinkList return error

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenError.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenError.kt
@@ -1,8 +1,10 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
-fun DrinkListScreenError(){
-
+fun DrinkListScreenError(state: DrinkListContract.State.Error) {
+    Text(text = state.error)
 }
