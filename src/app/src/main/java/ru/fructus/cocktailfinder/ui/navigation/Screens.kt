17dea20aec843821087f96328fb8cc7d8b44e61e
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:31:19 +0300

    add navigation destination for DrinkListScreen and DrinkDetailsScreen

---
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/navigation/Screens.kt
@@ -0,0 +1,17 @@
+package ru.fructus.cocktailfinder.ui.navigation
+
+interface NavigationDestination{
+    val route: String
+    val titleScreen: Int?
+}
+object DrinkListScreen : NavigationDestination {
+    override val route: String = "drink_list_screen"
+    override val titleScreen: Int? = null
+}
+
+object DrinkDetailsScreen : NavigationDestination {
+    override val route: String = "drink_list_screen"
+    override val titleScreen: Int? = null
+    const val drinkId = "drinkId"
+    val routeWithArgs = "$route/{$drinkId}"
+}
