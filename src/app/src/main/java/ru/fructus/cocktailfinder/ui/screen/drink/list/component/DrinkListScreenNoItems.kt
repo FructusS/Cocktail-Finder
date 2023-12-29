bed4f57375d651e9ecb06070e3c8007d6a71ce40
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-30 00:38:44 +0300

    showing no items

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenNoItems.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenNoItems.kt
@@ -1,8 +1,10 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
-fun DrinkListScreenNoItems(){
-
+fun DrinkListScreenNoItems(state: DrinkListContract.State) {
+    Text(text = "no items")
 }
