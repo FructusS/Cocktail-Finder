1221525bf85e2f8ff6abc84cf083c7867d62bff5
Mihkail Afanasev <afanasev10056@gmail.com>
2023-12-31 01:57:10 +0300

    change content width CircularProgressIndicator

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenLoading.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenLoading.kt
@@ -1,10 +1,13 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
 fun DrinkListScreenLoading(state: DrinkListContract.State) {
-    CircularProgressIndicator()
+    CircularProgressIndicator(modifier = Modifier.wrapContentWidth().wrapContentHeight())
 }
