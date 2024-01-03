feb0552e9d645a4f62b96a88b4d561290dec71f0
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-04 02:02:13 +0300

    add refresh button

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenError.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenError.kt
@@ -1,10 +1,25 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import ru.fructus.cocktailfinder.R
 import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 
 @Composable
-fun DrinkListScreenError(state: DrinkListContract.State.Error) {
-    Text(text = state.error)
+fun DrinkListScreenError(state: DrinkListContract.State.Error, getRandomDrink: () -> Unit) {
+    Box(contentAlignment = Alignment.Center){
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = state.error, textAlign = TextAlign.Center)
+            Button(onClick = { getRandomDrink() }) {
+                Text(text = stringResource(id = R.string.retry))
+            }
+        }
+    }
+
 }
