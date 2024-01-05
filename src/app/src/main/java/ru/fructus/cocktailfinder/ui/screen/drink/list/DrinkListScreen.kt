4911d27d483d9f106f5965450131abde9da685c0
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-06 00:12:09 +0300

    add PullRefreshIndicator

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListScreen.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListScreen.kt
@@ -1,16 +1,34 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.collectLatest
 import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenError
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenLoading
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenNoItems
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenSuccess
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DrinkListScreen(
     viewModel: DrinkListViewModel = hiltViewModel(), navigateToDrinkDetailsScreen: () -> Unit
@@ -21,7 +39,27 @@ fun DrinkListScreen(
         viewModel.event(DrinkListContract.Event.OnEnterScreen)
     }
 
-    Surface() {
+    Surface(
+        modifier = Modifier
+            .pullRefresh(pullToRefreshState)
+            .verticalScroll(rememberScrollState())
+    ) {
+        when (effect) {
+            DrinkListContract.Effect.PullToRefresh -> {
+                Box(modifier = Modifier.zIndex(1000f)
+                ) {
+                    PullRefreshIndicator(
+                        isPullToRefresh,
+                        pullToRefreshState,
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .wrapContentHeight()
+                            .align(Alignment.TopCenter)
+                    )
+                }
+            }
+        }
+
         when (val currentState = state.value) {
             is DrinkListContract.State.Success -> {
                 DrinkListScreenSuccess(state = currentState, viewModel)
