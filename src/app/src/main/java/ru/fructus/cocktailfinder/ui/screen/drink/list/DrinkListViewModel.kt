f60ea6c9ca68aae92b91b024c2b16e3c5312f322
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-06 00:11:40 +0300

    add effect

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListViewModel.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
