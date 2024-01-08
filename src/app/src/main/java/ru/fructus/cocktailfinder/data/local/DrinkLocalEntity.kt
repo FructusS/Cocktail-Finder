4c8f36670938ffd3b8821292fef00bdfc679c711
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-08 15:10:20 +0300

    local entity for room database

---
+++ src/app/src/main/java/ru/fructus/cocktailfinder/data/local/DrinkLocalEntity.kt
@@ -0,0 +1,55 @@
+package ru.fructus.cocktailfinder.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.fructus.cocktailfinder.domain.DrinkDto
+
+@Entity(tableName = "drinks")
+data class DrinkLocalEntity(
+    @PrimaryKey val idDrink: Int,
+    val strAlcoholic: String,
+    val strCategory: String,
+    val strDrink: String,
+    val strDrinkThumb: String,
+    val strGlass: String,
+    val strIngredient1: String?,
+    val strIngredient10: String?,
+    val strIngredient11: String?,
+    val strIngredient12: String?,
+    val strIngredient13: String?,
+    val strIngredient14: String?,
+    val strIngredient15: String?,
+    val strIngredient2: String?,
+    val strIngredient3: String?,
+    val strIngredient4: String?,
+    val strIngredient5: String?,
+    val strIngredient6: String?,
+    val strIngredient7: String?,
+    val strIngredient8: String?,
+    val strIngredient9: String?,
+    val isFavorite: Boolean = false,
+){
+    fun toDrinkDto() = DrinkDto(
+        id = idDrink,
+        type = strAlcoholic,
+        category = strCategory,
+        title = strDrink,
+        typeGlass = strGlass,
+        image = strDrinkThumb,
+        strIngredient1 = strIngredient1,
+        strIngredient2 = strIngredient2,
+        strIngredient3 = strIngredient3,
+        strIngredient4 = strIngredient4,
+        strIngredient5 = strIngredient5,
+        strIngredient6 = strIngredient6,
+        strIngredient7 = strIngredient7,
+        strIngredient8 = strIngredient8,
+        strIngredient9 = strIngredient9,
+        strIngredient10 = strIngredient10,
+        strIngredient11 = strIngredient11,
+        strIngredient12 = strIngredient12,
+        strIngredient13 = strIngredient13,
+        strIngredient14 = strIngredient14,
+        strIngredient15 = strIngredient15
+    )
+}
