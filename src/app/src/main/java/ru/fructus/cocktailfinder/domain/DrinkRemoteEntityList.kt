15caa66aca9c0f4256596368bbdb8bde09e98d9c
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-08 15:09:50 +0300

    renamed from DrinkListEntity.kt

--- src/app/src/main/java/ru/fructus/cocktailfinder/domain/DrinkRemoteEntityList.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/domain/DrinkRemoteEntityList.kt
@@ -2,7 +2,7 @@ package ru.fructus.cocktailfinder.domain
 
 import com.google.gson.annotations.SerializedName
 
-data class DrinkListEntity(
+data class DrinkRemoteEntityList(
     @SerializedName("drinks")
-    val drinks : List<DrinkEntity>
+    val drinks : List<DrinkRemoteEntity>
 )
