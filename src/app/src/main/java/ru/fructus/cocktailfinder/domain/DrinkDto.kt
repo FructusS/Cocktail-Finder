4139e5a1ec1b1e8331ed7b314f54b7ef82e54a9b
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-08 15:18:39 +0300

    add ingredients field and mapper to DrinkLocalEntity

--- src/app/src/main/java/ru/fructus/cocktailfinder/domain/DrinkDto.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/domain/DrinkDto.kt
@@ -1,5 +1,7 @@
 package ru.fructus.cocktailfinder.domain
 
+import ru.fructus.cocktailfinder.data.local.DrinkLocalEntity
+
 data class DrinkDto(
     val id: Int,
     val title: String,
@@ -7,5 +9,62 @@ data class DrinkDto(
     val typeGlass: String,
     val image: String,
     var isFavorite: Boolean = false,
-    val category: String
-)
+    val category: String,
+    val strIngredient1: String?,
+
+    val strIngredient10: String?,
+
+    val strIngredient11: String?,
+
+    val strIngredient12: String?,
+
+    val strIngredient13: String?,
+
+    val strIngredient14: String?,
+
+    val strIngredient15: String?,
+
+    val strIngredient2: String?,
+
+    val strIngredient3: String?,
+
+    val strIngredient4: String?,
+
+    val strIngredient5: String?,
+
+    val strIngredient6: String?,
+
+    val strIngredient7: String?,
+
+    val strIngredient8: String?,
+
+    val strIngredient9: String?,
+) {
+    fun toDrinkLocalEntity(): DrinkLocalEntity {
+        return DrinkLocalEntity(
+            idDrink = id,
+            strAlcoholic = type,
+            strCategory = category,
+            strDrink = title,
+            isFavorite = isFavorite,
+            strGlass = typeGlass,
+            strDrinkThumb = image,
+            strIngredient1 = strIngredient1,
+            strIngredient2 = strIngredient2,
+            strIngredient3 = strIngredient3,
+            strIngredient4 = strIngredient4,
+            strIngredient5 = strIngredient5,
+            strIngredient6 = strIngredient6,
+            strIngredient7 = strIngredient7,
+            strIngredient8 = strIngredient8,
+            strIngredient9 = strIngredient9,
+            strIngredient10 = strIngredient10,
+            strIngredient11 = strIngredient11,
+            strIngredient12 = strIngredient12,
+            strIngredient13 = strIngredient13,
+            strIngredient14 = strIngredient14,
+            strIngredient15 = strIngredient15
+        )
+    }
+
+}
