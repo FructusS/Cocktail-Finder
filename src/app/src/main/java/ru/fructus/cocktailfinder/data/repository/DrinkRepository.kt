a4c5332d706c06e908da4c36415ff2ab43be9d55
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-08 15:09:32 +0300

    add realization updateDrink and getDrinkList from drink dao

--- src/app/src/main/java/ru/fructus/cocktailfinder/data/repository/DrinkRepository.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/data/repository/DrinkRepository.kt
@@ -1,15 +1,23 @@
 package ru.fructus.cocktailfinder.data.repository
 
+import ru.fructus.cocktailfinder.data.local.DrinkDao
+import ru.fructus.cocktailfinder.data.local.DrinkLocalEntity
 import ru.fructus.cocktailfinder.data.remote.DrinkApi
 import ru.fructus.cocktailfinder.domain.DrinkDto
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class DrinkRepository @Inject constructor(private val drinkApi: DrinkApi) {
+class DrinkRepository @Inject constructor(private val drinkApi: DrinkApi, private val drinkDao: DrinkDao) {
 
-    suspend fun getRandomDrinkList() : List<DrinkDto>?{
-        return drinkApi.getRandomDrinkList()?.drinks?.map { it.toDrinkDto() }
+    suspend fun getRandomDrink() : DrinkDto? {
+        return drinkApi.getRandomDrink()?.drinks?.get(0)?.toDrinkDto()
+
     }
 
+
+    suspend fun getDrinkList() : List<DrinkDto>? {
+        return drinkDao.getDrinkList()?.map { it.toDrinkDto() }
+    }
+    suspend fun updateDrink(drinkLocalEntity: DrinkLocalEntity) = drinkDao.updateDrink(drinkLocalEntity)
 }
