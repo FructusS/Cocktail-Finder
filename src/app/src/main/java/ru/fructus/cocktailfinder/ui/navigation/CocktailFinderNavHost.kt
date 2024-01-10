24fd0079d71557c5a0ad31aad515a15f426a00fe
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-11 02:46:04 +0300

    add DrinkDetailsDestination

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/navigation/CocktailFinderNavHost.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/navigation/CocktailFinderNavHost.kt
@@ -4,19 +4,26 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import ru.fructus.cocktailfinder.ui.screen.drink.details.DrinkDetailsScreen
 import ru.fructus.cocktailfinder.ui.screen.drink.list.DrinkListScreen
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CocktailFinderNavHost(
-    navController: NavHostController, startDestination: String = DrinkListScreen.route
+    navController: NavHostController, startDestination: String = DrinkListDestination.route
 ) {
     NavHost(
         navController = navController, startDestination = startDestination
     ) {
-        composable(DrinkListScreen.route) {
+        composable(DrinkDetailsDestination.route) {
+            DrinkDetailsScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(DrinkListDestination.route) {
             DrinkListScreen(navigateToDrinkDetailsScreen = {
                 navController.navigate(
-                    DrinkDetailsScreen.route
+                    DrinkDetailsDestination.route
                 )
             })
         }
