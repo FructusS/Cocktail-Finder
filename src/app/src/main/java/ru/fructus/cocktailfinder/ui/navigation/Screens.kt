9833b6fcb60ece1e27a31af390a9f07df6514871
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-11 02:43:37 +0300

    rename from DrinkDetailsScreen to DrinkDetailsDestination delete drinkId and routeWithArgs val rename route from drink_list_screen to drink_details_screen

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/navigation/Screens.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/navigation/Screens.kt
@@ -9,9 +9,8 @@ object DrinkListDestination : NavigationDestination {
     override val route: String = "drink_list_screen"
     override val titleScreen: Int? = null
 }
-
-object DrinkDetailsScreen : NavigationDestination {
-    override val route: String = "drink_list_screen"
+object DrinkDetailsDestination : NavigationDestination {
+    override val route: String = "drink_details_screen"
     override val titleScreen: Int? = null
     const val drinkId = "drinkId"
     val routeWithArgs = "$route/{$drinkId}"
