a57537f517c5fd95f9e3fcff903497ccb010da97
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-11 03:16:44 +0300

    feat: display info about drinks as vertical grid with 2 columns drinks info: title and image

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenSuccess.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/component/DrinkListScreenSuccess.kt
@@ -1,23 +1,24 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import ru.fructus.cocktailfinder.R
-import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
+import ru.fructus.cocktailfinder.ui.screen.drink.list.DrinkListContract
 import ru.fructus.cocktailfinder.ui.screen.drink.list.DrinkListViewModel
 
 @OptIn(ExperimentalGlideComposeApi::class)
@@ -25,32 +26,28 @@ import ru.fructus.cocktailfinder.ui.screen.drink.list.DrinkListViewModel
 
 fun DrinkListScreenSuccess(state: DrinkListContract.State.Success, viewModel: DrinkListViewModel) {
 
-    state.list.forEach { drink ->
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(), contentAlignment = Alignment.Center
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth()
-            ) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(
+            3.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        items(state.drinks) { drink ->
+            Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = Modifier.clickable {
+
+            }) {
+                Text(
+                    text = drink.title, textAlign = TextAlign.Center
+                )
+
                 GlideImage(
                     model = drink.image,
                     contentDescription = "Image ${drink.image}",
                     contentScale = ContentScale.Fit,
                     loading = placeholder(R.drawable.ic_downloading)
                 )
-                Text(text = drink.title)
-                Text(text = stringResource(R.string.id, drink.id))
-                Text(text = drink.category)
-                Text(text = drink.type)
-                Text(text = drink.typeGlass)
-                Image(painter = if (drink.isFavorite) painterResource(R.drawable.ic_like) else painterResource(
-                    R.drawable.ic_dislike
-                ), contentDescription = "like button", modifier = Modifier.clickable {
-                    viewModel.event(DrinkListContract.Event.OnFavoriteClick(drink))
-                })
             }
         }
     }
