b111d1396c8682e888e796a58bb94745e4cbdedb
Mihkail Afanasev <afanasev10056@gmail.com>
2024-01-11 03:14:34 +0300

    feat: navigation to DrinkDetailsScreen and pull to refresh functional

--- src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListScreen.kt
+++ src/app/src/main/java/ru/fructus/cocktailfinder/ui/screen/drink/list/DrinkListScreen.kt
@@ -1,82 +1,84 @@
 package ru.fructus.cocktailfinder.ui.screen.drink.list
 
-import androidx.compose.foundation.layout.Box
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collectLatest
-import ru.fructus.cocktailfinder.ui.screen.drink.DrinkListContract
+import ru.fructus.cocktailfinder.R
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenError
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenLoading
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenNoItems
 import ru.fructus.cocktailfinder.ui.screen.drink.list.component.DrinkListScreenSuccess
 
-@OptIn(ExperimentalMaterialApi::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun DrinkListScreen(
     viewModel: DrinkListViewModel = hiltViewModel(), navigateToDrinkDetailsScreen: () -> Unit
 ) {
     val state = viewModel.state.collectAsState()
-    val effect by viewModel.effect.collectAsState(
-        DrinkListContract.Effect.PullToRefresh
-    )
-    var isPullToRefresh by remember {
-        mutableStateOf(false)
-    }
-    val pullToRefreshState = rememberPullRefreshState(refreshing = isPullToRefresh, onRefresh = {
-        viewModel.event(DrinkListContract.Event.OnRefresh)
-    })
+    val effect =
+        viewModel.effect.collectAsState(initial = DrinkListContract.Effect.OnRandomDrinkButtonClick)
+    val pullToRefreshState = rememberPullRefreshState(
+        refreshing = state.value is DrinkListContract.State.Loading,
+        onRefresh = {
+            viewModel.event(DrinkListContract.Event.OnRefresh)
+        })
     LaunchedEffect(key1 = state) {
         viewModel.event(DrinkListContract.Event.OnEnterScreen)
     }
     LaunchedEffect(key1 = effect) {
         viewModel.effect.collectLatest {
             when (it) {
-                is DrinkListContract.Effect.PullToRefresh -> {
-                    isPullToRefresh = !isPullToRefresh
+
+                DrinkListContract.Effect.OnRandomDrinkButtonClick -> {
+                    navigateToDrinkDetailsScreen()
                 }
             }
         }
     }
 
 
-    Surface(
-        modifier = Modifier
-            .pullRefresh(pullToRefreshState)
-            .verticalScroll(rememberScrollState())
-    ) {
-        when (effect) {
-            DrinkListContract.Effect.PullToRefresh -> {
-                Box(modifier = Modifier.zIndex(1000f)
-                ) {
-                    PullRefreshIndicator(
-                        isPullToRefresh,
-                        pullToRefreshState,
-                        modifier = Modifier
-                            .wrapContentWidth()
-                            .wrapContentHeight()
-                            .align(Alignment.TopCenter)
-                    )
-                }
-            }
+
+    Scaffold(modifier = Modifier.pullRefresh(pullToRefreshState), floatingActionButton = {
+        FloatingActionButton(onClick = {
+            viewModel.event(DrinkListContract.Event.OnRandomDrinkButtonClick)
+        }) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_random),
+                contentDescription = stringResource(
+                    id = R.string.random_drink
+                )
+            )
         }
+    }) {
+
+        PullRefreshIndicator(
+            state.value is DrinkListContract.State.Loading,
+            pullToRefreshState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth()
+                .wrapContentHeight()
+                .zIndex(100f)
+        )
 
         when (val currentState = state.value) {
             is DrinkListContract.State.Success -> {
@@ -85,7 +87,7 @@ fun DrinkListScreen(
 
             is DrinkListContract.State.Error -> {
                 DrinkListScreenError(state = currentState,
-                    getRandomDrink = { viewModel.getRandomDrink() })
+                    getRandomDrink = { viewModel.event(DrinkListContract.Event.OnRefresh) })
             }
 
             DrinkListContract.State.Loading -> {
@@ -93,7 +95,7 @@ fun DrinkListScreen(
             }
 
             DrinkListContract.State.NoItems -> {
-                DrinkListScreenNoItems(getRandomDrink = { viewModel.getRandomDrink() })
+                DrinkListScreenNoItems(getRandomDrink = { viewModel.event(DrinkListContract.Event.OnRefresh) })
             }
         }
     }
